83261dbf515e70ca61641d035b92da6bbac5ab13
kumar-saket-18 <79161970+kumar-saket-18@users.noreply.github.com>
2024-09-20 19:14:22 +0530

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -124,7 +124,7 @@ LANGUAGE_CODE = 'en-us'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 AUTH_USER_MODEL = 'users.User'
